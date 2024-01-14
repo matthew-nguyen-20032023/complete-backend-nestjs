52e04297349a1cc3d6f72776f3bae31b32f9e527
dylan123 <123>
2024-01-14 17:21:46 +0700

    feat: base validate module

---
+++ src/modules/validate/dto/get-hash-password.dto.ts
@@ -0,0 +1,7 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber } from "class-validator";
+import { Type } from "class-transformer";
+
+export class GetHashPasswordDto {
+
+}
---
+++ src/modules/validate/validate.const.ts
@@ -0,0 +1,7 @@
+export enum ValidateSuccessMessage {
+
+}
+
+export enum ValidateFailedMessage {
+
+}
