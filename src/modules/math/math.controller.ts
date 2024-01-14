7ce26c340f4592ee3ce86149107c8e99f154df62
dylan123 <123>
2024-01-14 17:09:15 +0700

    fix css

--- src/modules/math/math.controller.ts
+++ src/modules/math/math.controller.ts
@@ -1,4 +1,6 @@
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { Body, Controller, Get, HttpStatus, Post, Query } from "@nestjs/common";
+
 import { Public } from "src/modules/authentication/auth.const";
 import { MathService } from "src/modules/math/math.service";
 import { IResponseToClient } from "src/configs/response-to-client.config";
@@ -7,10 +9,14 @@ import { MathSuccessMessage } from "src/modules/math/math.const";
 import { MinusDto } from "src/modules/math/dto/minus.dto";
 
 @Controller("math")
+@ApiTags("Math")
 export class MathController {
   constructor(private readonly mathService : MathService) {}
 
   @Get("sum")
+  @ApiOperation({
+    summary: 'Api to calculate two number'
+  })
   @Public()
   async sumTwoNumber(@Query() sumDto: SumDto): Promise<IResponseToClient> {
     const data = await this.mathService.sumTwoNumber(sumDto.firstNumber, sumDto.secondNumber);
